00883ec45cc53ee26f37a3d40a69ff94ed75e273
hongjun <hongjun@nucarf.com>
2021-03-08 13:33:51 +0800

    修改web项目

--- businessServer/src/main/java/com/business/BusinessApplication.java
+++ businessServer/src/main/java/com/business/BusinessApplication.java
@@ -2,8 +2,10 @@ package com.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BusinessApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessApplication.class, args);
