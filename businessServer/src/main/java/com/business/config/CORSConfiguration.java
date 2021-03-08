98d0f8ec6702a403bdb38f3683497dbc49d1b4a0
hongjun <hongjun@nucarf.com>
2021-03-08 13:53:30 +0800

    修改web项目

--- businessServer/src/main/java/com/business/config/CORSConfiguration.java
+++ businessServer/src/main/java/com/business/config/CORSConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * 需要跨域支持
  */
+
 @Configuration
 public class CORSConfiguration extends WebMvcConfigurerAdapter{
 	@Override
