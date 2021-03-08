98823c646fd7bc8de1fe910179d2deb789a8f035
hongjun <hongjun@nucarf.com>
2021-03-08 13:49:45 +0800

    修改web项目

--- businessServer/src/main/java/com/business/config/ShiroConfiguration.java
+++ businessServer/src/main/java/com/business/config/ShiroConfiguration.java
@@ -12,6 +12,7 @@ import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class ShiroConfiguration {
 
